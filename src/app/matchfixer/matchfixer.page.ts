e270a97594dfd1e50ed5072d4c033f947d8786be
ishan sharma <ad912coolhridya@gmail.com>
2019-09-18 15:22:22 +0530

    fetching works

--- src/app/matchfixer/matchfixer.page.ts
+++ src/app/matchfixer/matchfixer.page.ts
@@ -15,6 +15,13 @@ export class MatchfixerPage implements OnInit {
   id2: any;
   url: string;
   fixture: any[]=[];
+  venue: any[]=[];
+  venueUrl: any;
+  player: any[]=[];
+  playerUrl: any;
+  toss: any[]=[];
+  teams: any[]=[];
+  //tossUrl: any;
   constructor(
     //private route: ActivatedRoute,
     //private router: Route
@@ -47,12 +54,71 @@ export class MatchfixerPage implements OnInit {
 
   goconsl(){
     console.log(''+this.id1+''+this.id2);
-    console.log(this.url);
+    // console.log(this.url);
+    // this.fetchVenue();
+    // this.fetchPlayer();
+    // this.fetchToss();
   }
 
   goback(){
     this.navCrl.pop();
     this.navCrl.navigateRoot('/home');
+  }
+
+  fetchVenue(){
+    this.fixture.forEach((item,index) => {
+      if(item.status!='NS'){  
+      this.venueUrl='https://cricppp.herokuapp.com/users/venue/'+item.venue_id;
+      this.http.get(this.venueUrl).subscribe(response => {
+        this.venue[index]=response["name"];
+        console.log('venue '+this.venue[index]+' '+index);
+      });
+    }
+    else
+    {
+      this.venue[index]='no venue/no match';
+      console.log('venue '+this.venue[index]+' '+index);
+    }
+    });
+  }
+
+  fetchPlayer(){
+    this.fixture.forEach((item,index) => {
+      if(item.status!='NS'){  
+      this.playerUrl='https://cricppp.herokuapp.com/users/player/'+item.man_of_match_id;
+      this.http.get(this.playerUrl).subscribe(response => {
+        this.player[index]=response["fullname"];
+        console.log('player '+this.player[index]+''+index);
+      });
+    }
+    else
+    {
+      this.player[index]='no pop/no match';
+      console.log('player '+this.player[index]+''+index);
+    }
+    });
+  }
+
+  fetchToss(){
+    this.fixture.forEach((item,index) => {
+      if(item.status!='NS'){  
+      //this.tossUrl='https://cricppp.herokuapp.com/users/player/'+item.man_of_match_id;
+      this.http.get('https://cricppp.herokuapp.com/users/teams').subscribe(response => {
+        this.teams=response["teams"];
+        this.teams.forEach((itm,indx) => {
+          if(itm.id==item.toss_won_team_id){
+            this.toss[index]=itm.name;
+            console.log('toss '+this.toss[index]+''+index);
+          }
+        });
+      });
+    }
+    else
+    {
+      this.toss[index]='no toss/no match';
+      console.log('toss '+this.toss[index]+''+index);
+    }
+    });
   }
 
   ngOnInit() {
@@ -69,8 +135,13 @@ export class MatchfixerPage implements OnInit {
         // console.log(params.toString());
         this.fixture = response["allFixtures"];
         console.log(this.fixture[0].note);
+        this.fetchVenue();
+        this.fetchPlayer();
+        this.fetchToss();
         });
-        // xxx;
+        //this.fetchVenue();
+        //this.fetchPlayer();
+        //this.fetchToss();
     });
   }
 
